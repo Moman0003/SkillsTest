2207acc090a9681bf6bcc005220362e3dec1a6d8
momans <mos003@edu.zealand.dk>
2024-06-14 10:43:59 +0200

    First commit

--- SkillsTest/obj/Debug/net8.0/SkillsTest.AssemblyInfo.cs
+++ SkillsTest/obj/Debug/net8.0/SkillsTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SkillsTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7821e8e9de4b066857c57d73fe32f184cff94d1e")]
 [assembly: System.Reflection.AssemblyProductAttribute("SkillsTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SkillsTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
